74bb301396791b7474a436f6deadc995a6a68886
TonyYang9527 <leetony554@gmail.com>
2017-04-04 22:20:30 +0800

    index code add

---
+++ src/main/java/com/cell/user/web/controller/system/index/IndexController.java
@@ -0,0 +1,13 @@
+package com.cell.user.web.controller.system.index;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cell.user.web.support.BeanSupport;
+
+
+@RestController
+@RequestMapping("/admin/user")
+public class IndexController extends BeanSupport {
+
+}
---
+++ src/main/java/com/cell/user/web/controller/system/index/LoginController.java
@@ -0,0 +1,13 @@
+package com.cell.user.web.controller.system.index;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cell.user.web.support.BeanSupport;
+
+
+@RestController
+@RequestMapping("/login")
+public class LoginController extends BeanSupport {
+
+}
