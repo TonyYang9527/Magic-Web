88b867131ee3a34a91fbebb1f048e92455313276
TonyYang9527 <leetony554@gmail.com>
2017-04-04 21:50:48 +0800

    demo code delete

--- src/main/java/com/cell/user/web/controller/system/user/SysUserController.java
+++ src/main/java/com/cell/user/web/controller/system/user/SysUserController.java
@@ -14,23 +14,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.alibaba.fastjson.JSON;
-import com.cell.user.ifacade.facade.GetRoleFacade;
 import com.cell.user.ifacade.facade.SysAuthorityFacade;
-import com.cell.user.ifacade.request.GetRoleReq;
-import com.cell.user.ifacade.response.GetRoleRsp;
-import com.cell.user.vo.single.RoleVo;
 import com.cell.user.web.support.BeanSupport;
 import com.cell.user.web.support.Result;
-import com.cell.user.web.support.RetCodeConst;
 
 @Api(tags = { "消息服务" })
 @RestController
 @RequestMapping("/admin/user")
 public class SysUserController extends BeanSupport {
-
-	@Resource
-	private GetRoleFacade getRoleFacade;
 
 	@Resource
 	private SysAuthorityFacade authorityService;
@@ -57,20 +48,21 @@ public class SysUserController extends BeanSupport {
 	@ApiOperation(value = "删除消息")
 	@RequestMapping(value = "/get", method = RequestMethod.POST)
 	@ResponseBody
-	public Result<RoleVo> getRoleById(
+	public Result<?> getRoleById(
 			@RequestParam(value = "id", required = true) Long id) {
-		if (id == null)
-			return fail("id异常");
-		// // 获取远程服务代理
-		GetRoleReq req = new GetRoleReq();
-		req.setId(new Long(105));
-		GetRoleRsp rsp = getRoleFacade.getRole(req);
-		logger.info("req:{},rsp:{}", req, rsp);
-		if (rsp == null || RetCodeConst.FAIL.equals(rsp.getRetCode())) {
-			return fail("返回异常");
-		}
-		
-		return success(rsp.getRole());
+//		if (id == null)
+//			return fail("id异常");
+//		// // 获取远程服务代理
+//		GetRoleReq req = new GetRoleReq();
+//		req.setId(new Long(105));
+//		GetRoleRsp rsp = getRoleFacade.getRole(req);
+//		logger.info("req:{},rsp:{}", req, rsp);
+//		if (rsp == null || RetCodeConst.FAIL.equals(rsp.getRetCode())) {
+//			return fail("返回异常");
+//		}
+//		
+//		return success(rsp.getRole());
+		return  null;
 	}
 
 	/**
@@ -81,15 +73,16 @@ public class SysUserController extends BeanSupport {
 	@ApiOperation(value = "删除消息")
 	@RequestMapping(value = "/test", method = RequestMethod.POST)
 	@ResponseBody
-	public Result<RoleVo> test() {
+	public Result<?> test() {
 		// 获取远程服务代理
-		GetRoleReq req = new GetRoleReq();
-		req.setId(new Long(105));
-		GetRoleRsp rsp = getRoleFacade.getRole(req);
-		String result = JSON.toJSONString(rsp);
-		logger.info("req:{},rsp:{},result:{}", req, rsp, result);
-		if (rsp == null || RetCodeConst.FAIL.equals(rsp.getRetCode()))
-			return fail("返回异常");
-		return success(rsp.getRole());
+//		GetRoleReq req = new GetRoleReq();
+//		req.setId(new Long(105));
+//		GetRoleRsp rsp = getRoleFacade.getRole(req);
+//		String result = JSON.toJSONString(rsp);
+//		logger.info("req:{},rsp:{},result:{}", req, rsp, result);
+//		if (rsp == null || RetCodeConst.FAIL.equals(rsp.getRetCode()))
+//			return fail("返回异常");
+//		return success(rsp.getRole());
+		return  null;
 	}
 }
