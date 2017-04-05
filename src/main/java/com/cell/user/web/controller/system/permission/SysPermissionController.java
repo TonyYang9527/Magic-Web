d977e3174c5ce079f7a019f4c2b05112b297f2b0
TonyYang9527 <leetony554@gmail.com>
2017-04-05 22:13:10 +0800

    unuse code delete

--- src/main/java/com/cell/user/web/controller/system/permission/SysPermissionController.java
+++ src/main/java/com/cell/user/web/controller/system/permission/SysPermissionController.java
@@ -1,7 +1,5 @@
 package com.cell.user.web.controller.system.permission;
 
-import io.swagger.annotations.Api;
-
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cell.user.ifacade.facade.SysPermissionFacade;
 import com.cell.user.web.support.BeanSupport;
 
-@Api(tags = { "消息服务" })
 @RestController
 @RequestMapping("/admin/permission")
 public class SysPermissionController extends BeanSupport {
